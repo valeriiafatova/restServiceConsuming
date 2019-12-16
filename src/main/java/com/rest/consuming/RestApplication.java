3bcea1beb1f0996dde21f3a7665f53648fe2a99c
Valeriia Fatova <valeriia_fatova@epam.com>
2019-12-16 13:12:41 +0200

    03 Consume Rest

--- src/main/java/com/rest/consuming/RestApplication.java
+++ src/main/java/com/rest/consuming/RestApplication.java
@@ -1,12 +1,33 @@
 package com.rest.consuming;
 
+import com.rest.consuming.entity.Course;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(RestApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Course course = restTemplate.getForObject("http://localhost:8080/courses/2", Course.class);
+            LOG.info("We are on course: {}", course.getTitle());
+        };
     }
 }

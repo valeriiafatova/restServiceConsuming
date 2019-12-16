a3b2096a630892005d320b5b1ce6ae4d82863405
Valeriia Fatova <valeriia_fatova@epam.com>
2019-12-16 13:06:58 +0200

    02 Add resource to consume

---
+++ src/main/java/com/rest/consuming/entity/Course.java
@@ -0,0 +1,33 @@
+package com.rest.consuming.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Course {
+    private Long id;
+    private String title;
+
+    public Course(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

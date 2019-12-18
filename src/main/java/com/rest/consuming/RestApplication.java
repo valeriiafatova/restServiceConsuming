1c397a003792589177b5b2f162eba8ffcad4df4f
Viacheslav Skrypets <Viacheslav_Skrypets@epam.com>
2019-12-18 12:35:18 +0200

    Add consumer for our WebService

--- src/main/java/com/rest/consuming/RestApplication.java
+++ src/main/java/com/rest/consuming/RestApplication.java
@@ -1,11 +1,35 @@
 package com.rest.consuming;
 
+import com.rest.consuming.entity.Activity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class RestApplication {
+    private static final Logger log = LoggerFactory.getLogger(RestApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Activity activity = restTemplate.getForObject(
+                    "http://localhost:8080/activities/1", Activity.class);
+            log.info(activity.toString());
+        };
     }
 }
